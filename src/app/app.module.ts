a00d4279fd4c6e49cc49f4191f620c32bc628071
kishanrajg16 <gkishan2011@gmail.com>
2021-08-27 22:27:28 +0530

    const DISHES initialized, menu.component.html modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -20,6 +22,8 @@ import { MenuComponent } from './menu/menu.component';
     MatListModule,
     MatGridListModule,
     FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+const DISHES: Dish[] = [
+{
+  id: '0',
+  name: 'Burger Meal',
+  image: '/assets/images/burgerfries.jpeg',
+  category: 'mains',
+  featured: true,
+  label: '',
+  price: '8.99',
+  description: 'Italian burger served with crispy French fries and 500ml of chilled Coke / Soda.'
+},
+{
+  id: '1',
+  name: 'Pizza',
+  image: '/assets/images/pizza.jpeg',
+  category: 'mains',
+  featured: false,
+  label: '',
+  price: '4.99',
+  description: 'Regular sized pizza - 10" with Mushroom, Tomato, olives, jalapenos, onion, american corn and green capsicum as its toppings.'
+},
+{
+  id: '2',
+  name: 'Noodles',
+  image: '/assets/images/noodles.jpeg',
+  category: 'mains',
+  featured: false,
+  label: '',
+  price: '3.99',
+  description: 'Long/Flat Chinese Noodles with Onion, Tomato, Carrot, Mushroom and Green Capsicum as toppings served with Veg Manchow Soup / Manchurian Sauce.'
+},
+{
+  id: '3',
+  name: 'Club Sandwiches',
+  image: '/assets/images/sandwiches.jpeg',
+  category: 'mains',
+  featured: false,
+  label: '',
+  price: '2.99',
+  description: 'Street Style Sandwiches with Cucumber, Onions, Capsicum, Tomato, Carrots mixed in Mayo with Cheese Slices.'
+}
+];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,48 +51,9 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-    {
-      id: '0',
-      name: 'Burger Meal',
-      image: '/assets/images/burgerfries.jpeg',
-      category: 'mains',
-      featured: true,
-      label: '',
-      price: '8.99',
-      description: 'Italian burger served with crispy French fries and 500ml of chilled Coke / Soda.'
-    },
-    {
-      id: '1',
-      name: 'Pizza',
-      image: '/assets/images/pizza.jpeg',
-      category: 'mains',
-      featured: false,
-      label: '',
-      price: '4.99',
-      description: 'Regular sized pizza - 10" with Mushroom, Tomato, olives, jalapenos, onion, american corn and green capsicum as its toppings.'
-    },
-    {
-      id: '2',
-      name: 'Noodles',
-      image: '/assets/images/noodles.jpeg',
-      category: 'mains',
-      featured: false,
-      label: '',
-      price: '3.99',
-      description: 'Long/Flat Chinese Noodles with Onion, Tomato, Carrot, Mushroom and Green Capsicum as toppings served with Veg Manchow Soup / Manchurian Sauce.'
-    },
-    {
-      id: '3',
-      name: 'Club Sandwiches',
-      image: '/assets/images/sandwiches.jpeg',
-      category: 'mains',
-      featured: false,
-      label: '',
-      price: '2.99',
-      description: 'Street Style Sandwiches with Cucumber, Onions, Capsicum, Tomato, Carrots mixed in Mayo with Cheese Slices.'
-    }
-   ];
+  dishes: Dish[] = DISHES;
+
+  selectedDish: Dish = DISHES[0];
 
   constructor() { }
 
