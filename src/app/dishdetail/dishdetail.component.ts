7d3d1f240ad4687b91cd14cb8e579c29466891f5
kishanrajg16 <gkishan2011@gmail.com>
2021-08-31 12:43:12 +0530

    const dish created, dishdetail card added to the template - task 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,11 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+const DISH = {
+  id: '1',
+  name: 'Pizza',
+  image: '/assets/images/pizza.jpeg',
+  category: 'mains',
+  featured: false,
+  label: '',
+  price: '4.99',
+  // tslint:disable-next-line:max-line-length
+  description: 'Regular sized pizza - 10" with Mushroom, Tomato, olives, jalapenos, onion, american corn and green capsicum as its toppings.',
+  comments: [
+       {
+           rating: 5,
+           comment: 'Imagine all the eatables, living in conFusion!',
+           author: 'John Lemon',
+           date: '2012-10-16T17:57:28.556094Z'
+       },
+       {
+           rating: 4,
+           comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+           author: 'Paul McVites',
+           date: '2014-09-05T17:57:28.556094Z'
+       },
+       {
+           rating: 3,
+           comment: 'Eat it, just eat it!',
+           author: 'Michael Jaikishan',
+           date: '2015-02-13T17:57:28.556094Z'
+       },
+       {
+           rating: 4,
+           comment: 'Ultimate, Reaching for the stars!',
+           author: 'Ringo Starry',
+           date: '2013-12-02T17:57:28.556094Z'
+       },
+       {
+           rating: 2,
+           comment: 'It\'s your birthday, we\'re gonna party!',
+           author: '25 Cent',
+           date: '2011-12-02T17:57:28.556094Z'
+       }
+   ]
+};
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
+
+  dish = DISH;
 
   constructor() { }
 
